1e9684216837c34058a55080db3ab1739fc68159
Prince05Pathak <102548615+Prince05Pathak@users.noreply.github.com>
2024-10-18 14:12:08 +0530

    script.py

--- script.py
+++ script.py
@@ -12,12 +12,9 @@ import string
 
 def set_api_keys():
     # Setting environment variables for API keys
-    OPENAIAPIKEY = "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"
-    LLAMACLOUDAPIKEY = "llx-Ep3RATyCgmuHsVzvDMz30xYXbd8fqmQDjQk3fC7lRJbP2S4Q"
-    os.environ["LLAMA_CLOUD_API_KEY"] = LLAMACLOUDAPIKEY
-    os.environ["OPENAI_API_KEY"] = OPENAIAPIKEY
+    LLAMACLOUDAPIKEY = os.getenv("LLAMA_CLOUD_API_KEY") 
+    OPENAIAPIKEY = os.getenv("OPENAI_API_KEY")
     return OPENAIAPIKEY
-
 
 def parse_pdf_to_markdown(filepath, output_path):
     # Parsing PDF to markdown using LlamaParse
